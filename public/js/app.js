973a503357ba29b1663da52c7b7f533e98875625
Ajith Pai <43682209+ajithpai07@users.noreply.github.com>
2020-02-03 09:20:00 +0530

    Registration.html css dix

--- public/js/app.js
+++ public/js/app.js
@@ -25,7 +25,11 @@ function getUiConfig() {
     'tosUrl': 'https://www.google.com'
   };
 }
-
+firebase.auth().onAuthStateChanged(user => {
+  if(user) {
+    window.location = 'registeration.html'; //After successful login, user will be redirected to registeration.html
+  }
+});
 var ui = new firebaseui.auth.AuthUI(firebase.auth());
 
 var handleSignedInUser = function(user) {
@@ -39,11 +43,7 @@ var handleSignedOutUser = function() {
   document.getElementById('user-signed-out').style.display = 'block';
   ui.start('#firebaseui-container', getUiConfig());
 };
-firebase.auth().onAuthStateChanged(user => {
-  if(user) {
-    window.location = 'registeration.html'; //After successful login, user will be redirected to registeration.html
-  }
-});
+
 firebase.auth().onAuthStateChanged(function(user) {
   document.getElementById('loading').style.display = 'none';
   document.getElementById('loaded').style.display = 'block';
