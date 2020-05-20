f2130c49d852d7676dfe8287d103cc36951c4de9
Srekar <eskalasrekar@gmail.com>
2020-05-20 18:33:12 +0530

    KYCstatusTagAdded

--- public/js/login.js
+++ public/js/login.js
@@ -61,5 +61,8 @@ loginForm.addEventListener('submit',(e) =>{
     .catch(function(error) {
       const fd12=document.querySelector("#fld12");
       fd12.textContent="*Invalid username or password";
+      // setTimeout(function() {
+      //   window.location="3_login.html";
+      // },3000);
     });
 });
